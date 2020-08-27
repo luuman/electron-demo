d107baf807a6b69df4cbe4b30c4e69a734a2a088
luuman <luuman@luuman.local>
2020-08-27 22:33:52 +0800

    webpack ipc

--- src/main.js
+++ src/main.js
@@ -2,5 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { ipcRenderer } from 'electron'
 
 createApp(App).use(store).use(router).mount('#app')
+
+console.log(ipcRenderer)
