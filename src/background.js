dfa9883c0feca75bc32dc9cf398f0b688c5ff2ca
luuman <luuman@luuman.local>
2020-09-02 14:30:44 +0800

    Zip done is code extractFile

--- src/background.js
+++ src/background.js
@@ -3,8 +3,10 @@ import Launcher from '@/main/Launcher'
 import { app, protocol, BrowserWindow, ipcMain, dialog, shell } from 'electron'
 import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
 import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer'
+import path from 'path'
+// import unzip from 'unzip'
 const isDevelopment = process.env.NODE_ENV !== 'production'
-
+process.noAsar = true
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win
@@ -109,62 +111,65 @@ ipcMain.handle('synchronous-message', (event, name) => {
 })
 // 启动下载
 ipcMain.on('download', (event, fileUrl) => {
-  const path = require('path')
-  win.webContents.session.on('will-download', (e, item) => {
-    // 获取文件的总大小
-    const totalBytes = item.getTotalBytes()
-    let fileBase = 0
-    // 设置文件的保存路径，此时默认弹出的 save dialog 将被覆盖
-    const filePath = path.join(app.getPath('downloads'), item.getFilename())
-    console.log(filePath)
-    item.setSavePath(filePath)
-    // 监听下载过程，计算并设置进度条进度
-    item.on('updated', () => {
-      let baifenb = item.getReceivedBytes() / totalBytes
-      if (fileBase !== baifenb) {
-        fileBase = baifenb
-        win.setProgressBar(baifenb)
-        event.reply('download-reply', baifenb, baifenb)
-      }
+  try {
+    console.log('download fly')
+    const path = require('path')
+    win.webContents.session.on('will-download', (e, item) => {
+      // 获取文件的总大小
+      const totalBytes = item.getTotalBytes()
+      let fileBase = 0
+      // 设置文件的保存路径，此时默认弹出的 save dialog 将被覆盖
+      const filePath = path.join(app.getPath('downloads'), item.getFilename())
+      console.log(filePath)
+      item.setSavePath(filePath)
+      // 监听下载过程，计算并设置进度条进度
+      item.on('updated', () => {
+        let baifenb = item.getReceivedBytes() / totalBytes
+        if (fileBase !== baifenb) {
+          fileBase = baifenb
+          if (win) win.setProgressBar(baifenb)
+          if (win) event.reply('download-reply', baifenb, baifenb)
+        }
+      })
+      // 监听下载结束事件
+      item.on('done', (e, state) => {
+        // 如果窗口还在的话，去掉进度条
+        if (win && !win.isDestroyed()) win.setProgressBar(-1)
+        // 下载被取消或中断了
+        if (state === 'interrupted') {
+          dialog.showErrorBox(
+            '下载失败',
+            `文件 ${item.getFilename()} 因为某些原因被中断下载`
+          )
+        }
+        // 下载完成，让 dock 上的下载目录Q弹一下下
+        if (state === 'completed') {
+          app.dock.downloadFinished(filePath)
+          // shell启动浏览器
+          // shell.openExternal('http://www.google.com')
+          // ipcRenderer.invoke('app-child', 'name')
+          shell.openPath(filePath).then(res => {
+            console.log('解压完毕')
+            event.reply('download-finish', filePath)
+          })
+        }
+      })
     })
-    // 监听下载结束事件
-    item.on('done', (e, state) => {
-      // 如果窗口还在的话，去掉进度条
-      if (!win.isDestroyed()) {
-        win.setProgressBar(-1)
-      }
-      // 下载被取消或中断了
-      if (state === 'interrupted') {
-        dialog.showErrorBox(
-          '下载失败',
-          `文件 ${item.getFilename()} 因为某些原因被中断下载`
-        )
-      }
-      // 下载完成，让 dock 上的下载目录Q弹一下下
-      if (state === 'completed') {
-        app.dock.downloadFinished(filePath)
-        // shell启动浏览器
-        // shell.openExternal('http://www.google.com')
-        // ipcRenderer.invoke('app-child', 'name')
-        shell.openPath(filePath).then(res => {
-          console.log('解压完毕')
-          event.reply('download-finish', filePath)
-        })
-      }
-    })
-  })
 
-  // const path = require('path')
-  // win.webContents.session.on('will-download', (event, item, webContents) => {
-  //   const filePath = path.join(app.getPath('downloads'), item.getFilename())
-  //   item.setSavePath(filePath)
-  // })
-  win.webContents.downloadURL(fileUrl)
+    // const path = require('path')
+    // win.webContents.session.on('will-download', (event, item, webContents) => {
+    //   const filePath = path.join(app.getPath('downloads'), item.getFilename())
+    //   item.setSavePath(filePath)
+    // })
+    win.webContents.downloadURL(fileUrl)
+  } catch (error) {
+    console.log(error)
+  }
 })
 const { spawn, exec } = require('child_process')
 // 打开应用
-ipcMain.on('openApp', (event, fileUrl) => {
-  exec('open -a "Motrix.app"', (error, stdout, stderr) => {
+ipcMain.on('openApp', (event, appName) => {
+  exec(`open -a "${appName}.app"`, (error, stdout, stderr) => {
     console.log(error, stdout, stderr)
   })
 })
@@ -186,6 +191,146 @@ ipcMain.on('appIs', (event, appName) => {
   log.stderr.on('end', () => {
   })
 })
+ipcMain.on('extractFile', (event, filePath, desPath) => {
+  const extract = require('extract-zip')
+  // const fs = require('fs')
+  try {
+    extract(filePath, { dir: path.join(desPath, 'extractFile') }, err => {
+      if (err) console.log('extractFile err: ', err)
+      // fs.writeFile(path.join(desPath, 'path.txt'), 'Electron.app/Contents/MacOS/Electron', err => {
+      //   if (err) console.log('extractFile err: ', err)
+      //   console.log('extractFile')
+      // })
+    })
+  } catch (error) {
+    console.log('catch : ', error)
+  }
+})
+
+ipcMain.on('compressing', (event, filePath, desPath) => {
+  console.log('compressing')
+  const compressing = require('compressing')
+  // 解压缩
+  compressing.zip.uncompress(filePath, desPath)
+    .then(() => {
+      console.log('success')
+    })
+    .catch(err => {
+      console.error(err)
+    })
+})
+ 
+ipcMain.on('DecompressZip', (event, filePath, desPath) => {
+  console.log('DecompressZip')
+  var DecompressZip = require('decompress-zip')
+  var unzipper = new DecompressZip(filePath)
+  // Add the error event listener
+  unzipper.on('error', function (err) {
+    console.log('Caught an error', err)
+  })
+
+  // Notify when everything is extracted
+  unzipper.on('extract', function (log) {
+    console.log('Finished extracting', log)
+  })
+
+  // Notify "progress" of the decompressed files
+  unzipper.on('progress', function (fileIndex, fileCount) {
+    console.log('Extracted file ' + (fileIndex + 1) + ' of ' + fileCount)
+  })
+  unzipper.extract({
+    path: desPath,
+    // You can filter the files that you want to unpack using the filter option
+    filter: function (file) {
+        console.log(file)
+        return file.type !== 'SymbolicLink'
+    }
+  })
+})
+
+ipcMain.on('unzip', (event, filePath, desPath) => {
+  console.log('unzip')
+})
+
+ipcMain.on('AdmZip', (event, filePath, desPath) => {
+  console.log('AdmZip')
+  var AdmZip = require('adm-zip')
+  const unzipper = new AdmZip(filePath)
+  unzipper.extractAllTo(desPath, true)
+  console.log('app.asar.unpacked.zip 解压缩完成')
+  console.log('unzipper.Extract({path: route}')
+})
+
+ipcMain.on('StreamZip', (event, filePath, desPath) => {
+  // invalid or unsupported zip format 不支持的格式
+  console.log('StreamZip')
+  const StreamZip = require('node-stream-zip')
+  const zip = new StreamZip({
+    file: filePath
+  })
+  zip.on('ready', () => {
+    console.log('Entries read: ' + zip.entriesCount);
+    for (const entry of Object.values(zip.entries())) {
+        const desc = entry.isDirectory ? 'directory' : `${entry.size} bytes`;
+        console.log(`Entry ${entry.name}: ${desc}`);
+    }
+    //读取完毕，记得关闭文件
+    zip.close()
+  });
+  // 报错提示
+  zip.on('error', err => {
+    console.log('unzip err：', err)
+  })
+})
+  // console.log(args, agd)
+  // const unzip = require('./unzip')
+  // var StreamZip = require('node-stream-zip');
+  // exec('ls -a', (error, stdout, stderr) => {
+  //   console.log(error, stdout, stderr)
+  // })
+  // var unzipper = new StreamZip({
+  //   file: args
+  // , storeEntries: true
+  // });
+  // console.log(unzipper)
+  // // unzipper.on('error', function (err) { console.error('[ERROR]', err); })
+  // unzipper.on("error", (err) => {
+  //   console.log("Unable to unzip the file " + args, err);
+  //   // return reject(err);
+  // });
+//   
+
+//   
+ 
+// unzipper.list();
+
+// packing a directory
+// tar.pack('./my-directory').pipe(fs.createWriteStream('my-tarball.tar'))
+
+// extracting a directory
+  
+  // fs.createReadStream(args).pipe(unzipper.Parse())
+    // .on('entry', function (entry) {
+    //   const fileName = entry.path;
+    //   const type = entry.type; // 'Directory' or 'File'
+    //   const size = entry.vars.uncompressedSize; // There is also compressedSize;
+    //   if (fileName === "this IS the file I'm looking for") {
+    //     entry.pipe(fs.createWriteStream('output/path'));
+    //   } else {
+    //     entry.autodrain();
+    //   }
+    // })
+  
+    // try {
+  //   let extract = unzip.Extract({ path: './' });
+  //   console.log(extract)
+  // } catch (error) {
+  //   console.log(error)
+  // }
+  
+  // 
+//   // fs.createReadStream(args).pipe(unzip.Extract({ path: 'output/path' }))
+// })
 
 global.launcher = new Launcher()
 console.log(global.launcher)
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="home">
     <div @click="download" v-show="!downIs && appIsShow">下载资源</div>
-    <div v-show="downIs">开始下载</div>
+    <div v-show="downIs">{{apiCon.downName}} 开始下载</div>
     <div v-show="appIsShow">APP 未安装</div>
     <div v-show="!appIsShow">APP 已安装</div>
     <div v-show="downIs">{{ parseFloat((arg * 100).toFixed(2)) }}%</div>
     <div v-show="filePath">文件下载地址：{{filePath}}</div>
     <proBar :numBase="arg"></proBar>
+    <div @click="unzip(item)" v-for="item in zip" :key="item">{{item}}</div>
   </div>
 </template>
 
@@ -24,13 +25,22 @@ export default {
   },
   data() {
     return {
+      zip: ['DecompressZip', 'unzip', 'AdmZip', 'StreamZip', 'compressing', 'extractFile'],
       arg: 0,
       downIs: false,
       appIsShow: false,
       appIsDone: '',
-      filePath: '',
-      downloadUrl: 'https://github.com/agalwood/Motrix/releases/download/v1.5.15/Motrix-1.5.15-mac.zip',
-      downName: 'Motrix'
+      // apiCon: {
+      //   downloadUrl: 'https://github.com/agalwood/Motrix/releases/download/v1.5.15/Motrix-1.5.15-mac.zip',
+      //   zipPath: '/Users/luuman/Downloads/Motrix-1.5.15-mac.zip',
+      //   downName: 'Motrix'
+      // },
+      apiCon: {
+        downloadUrl: 'https://github.com/michaelvillar/timer-app/releases/download/1.5.5/Timer.app.zip',
+        zipPath: '/Users/luuman/Downloads/Timer.app.zip',
+        downName: 'Timer'
+      },
+      filePath: ''
     }
   },
   mounted () {
@@ -39,15 +49,21 @@ export default {
   methods: {
     // app是否安装
     appIs () {
-      ipcRenderer.send('appIs', this.downName)
+      ipcRenderer.send('appIs', this.apiCon.downName)
       ipcRenderer.on('appIs-finish', (event, buffer) => {
         console.log(buffer)
         if (buffer) {
-          this.openApp(this.downName)
+          this.openApp(this.apiCon.downName)
           clearInterval(this.appIsDone)
           this.appIsShow = false
         } else this.appIsShow = true
       })
+    },
+    unzip (name) {
+      // ipcRenderer.send(name, '/Users/luuman/Downloads/Motrix-1.5.15-mac.zip', '/Users/luuman/Downloads/Motrix')
+      ipcRenderer.send(name, this.apiCon.zipPath, '/Users/luuman/Downloads')
+      // ipcRenderer.send(name, '/Users/luuman/Library/Caches/electron/httpsgithub.comelectronelectronreleasesdownloadv10.1.0electron-v10.1.0-darwin-x64.zip/electron-v10.1.0-darwin-x64.zip', '/Users/luuman/Downloads/Motrix')
+      // ipcRenderer.send(name, '/Users/luuman/Downloads/galshir.zip', '/Users/luuman/Downloads/Motrix')
     },
     // 打开app
     openApp (appName) {
@@ -56,7 +72,7 @@ export default {
     // 下载
     download () {
       this.downIs = true
-      ipcRenderer.send('download', this.downloadUrl)
+      ipcRenderer.send('download', this.apiCon.downloadUrl)
       ipcRenderer.on('download-reply', (event, arg) => {
         console.time()
         this.arg = arg
